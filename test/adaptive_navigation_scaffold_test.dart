4b34c4ed9fb6aa93321407f32b69c6f405e12fe1
Antonius Naumann <git@antonius.dev>
2022-05-24 15:14:15 +0200

    Initial commit

---
+++ test/adaptive_navigation_scaffold_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:adaptive_navigation_scaffold/adaptive_navigation_scaffold.dart';
+
+void main() {
+  test('', () {
+    // TODO: Add tests
+  });
+}
